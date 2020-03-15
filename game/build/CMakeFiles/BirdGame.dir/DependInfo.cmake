bb4c3576b34731b526e19823a20593e982a6bc21
SunTalk <infernal56727@gmail.com>
2020-03-15 19:01:52 +0800

    change to cmake

---
+++ game/build/CMakeFiles/BirdGame.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/SunTalk/Desktop/SunTalk/game/src/background.cpp" "C:/Users/SunTalk/Desktop/SunTalk/game/build/CMakeFiles/BirdGame.dir/src/background.cpp.obj"
+  "C:/Users/SunTalk/Desktop/SunTalk/game/src/button.cpp" "C:/Users/SunTalk/Desktop/SunTalk/game/build/CMakeFiles/BirdGame.dir/src/button.cpp.obj"
+  "C:/Users/SunTalk/Desktop/SunTalk/game/src/game.cpp" "C:/Users/SunTalk/Desktop/SunTalk/game/build/CMakeFiles/BirdGame.dir/src/game.cpp.obj"
+  "C:/Users/SunTalk/Desktop/SunTalk/game/src/main.cpp" "C:/Users/SunTalk/Desktop/SunTalk/game/build/CMakeFiles/BirdGame.dir/src/main.cpp.obj"
+  "C:/Users/SunTalk/Desktop/SunTalk/game/src/pillar.cpp" "C:/Users/SunTalk/Desktop/SunTalk/game/build/CMakeFiles/BirdGame.dir/src/pillar.cpp.obj"
+  "C:/Users/SunTalk/Desktop/SunTalk/game/src/role.cpp" "C:/Users/SunTalk/Desktop/SunTalk/game/build/CMakeFiles/BirdGame.dir/src/role.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_STATIC"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "C:/msys64/mingw32/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ game/build/CMakeFiles/BirdGame.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BirdGame.dir/src/main.cpp.obj"
+  "CMakeFiles/BirdGame.dir/src/game.cpp.obj"
+  "CMakeFiles/BirdGame.dir/src/background.cpp.obj"
+  "CMakeFiles/BirdGame.dir/src/button.cpp.obj"
+  "CMakeFiles/BirdGame.dir/src/pillar.cpp.obj"
+  "CMakeFiles/BirdGame.dir/src/role.cpp.obj"
+  "BirdGame.pdb"
+  "BirdGame.exe"
+  "BirdGame.exe.manifest"
+  "libBirdGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BirdGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
